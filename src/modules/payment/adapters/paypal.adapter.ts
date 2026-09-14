30d67a673561ef0855e80e4c8d35e12d322b1a6c
dinhvan <dinhvanle9xpro@gmail.com>
2026-09-14 11:29:27 +0700

    fix adapter

--- src/modules/payment/adapters/paypal.adapter.ts
+++ src/modules/payment/adapters/paypal.adapter.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { PayPalSDK } from '../external/paypal.sdk';
 import { PaymentProvider } from '../payment-provider.interface';
 
+@Injectable()
 export class PayPalAdapter implements PaymentProvider {
   constructor(private readonly paypal: PayPalSDK) {}
 
--- src/modules/payment/adapters/stripe.adapter.ts
+++ src/modules/payment/adapters/stripe.adapter.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { StripeSDK } from '../external/stripe.sdk';
 import { PaymentProvider } from '../payment-provider.interface';
 
+@Injectable()
 export class StripeAdapter implements PaymentProvider {
   constructor(private readonly stripe: StripeSDK) {}
 
