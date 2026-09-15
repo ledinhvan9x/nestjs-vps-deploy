e431fd377e4540bd2a3d5dcc0f75b0aabe57bb86
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-09-15 21:15:05 +0700

    Update paypal.sdk.ts

--- src/modules/payment/external/paypal.sdk.ts
+++ src/modules/payment/external/paypal.sdk.ts
@@ -2,6 +2,6 @@
 
 export class PayPalSDK {
   async createOrder(data: { total: number }) {
-    console.log(`PayPal: creating order ${data.total}`);
+    console.log(`PayPal creating order ${data.total}`);
   }
 }
